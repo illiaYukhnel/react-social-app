9bb22eb4c2bb187138888280cd19a3ba05568a3f
Illia Yukhnel <iluhajuhnel@gmail.com>
2023-02-12 12:34:38 +0200

    add logo and image, some css modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+import logo from './logo.svg'
+import image from './main_image.png'
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src={logo}/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src={image}/>
+                </div>
+                <div>
+                    avatar + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
